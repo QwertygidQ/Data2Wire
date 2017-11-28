a5b9403f3d3de6d9c36ce442db72450845903d0d
QwertygidQ <Qwertygid@gmail.com>
2017-11-28 23:18:50 +0300

    Added multiple encodings

---
+++ src/data2wire.py
@@ -0,0 +1,76 @@
+from src import encoding_algorithms
+import matplotlib.pyplot as plt
+
+enc_NRZ, enc_NRZI, enc_manch, enc_diff_manch, enc_MLT3, enc_2B1Q = \
+    1, 2, 3, 4, 5, 6
+
+
+def get_encoding_choice():
+    print('1) NRZ')
+    print('2) NRZI')
+    print('3) Manchester')
+    print('4) Differential Manchester')
+    print('5) MLT3')
+    print('6) 2B1Q\n')
+
+    return int(input('Enter your choice: '))
+
+
+def string_to_binary(string):
+    return [int(bit) for bit in ''.join(format(ord(ch), 'b') for ch in string)]
+
+
+def read_data():
+    binary = (input('Is your data binary? (Y/n) ').lower() != 'n')
+    data = input('Input data: ')
+
+    if binary:
+        for ch in data:
+            if ch != '0' and ch != '1':
+                raise ValueError('Data is not binary')
+
+        return [int(bit) for bit in data]
+    else:
+        return string_to_binary(data)
+
+
+def main():
+    while True:
+        print('Data2Wire by Qwertygid, 2017\n')
+        encoding = get_encoding_choice()
+        if encoding < enc_NRZ or encoding > enc_2B1Q:
+            print("Invalid choice\n")
+            continue
+
+        try:
+            data = read_data()
+        except ValueError as err:
+            print(err)
+            print()
+            continue
+
+        if encoding == enc_NRZ:
+            encoding_algorithms.encoding_NRZ(data)
+        elif encoding == enc_NRZI:
+            encoding_algorithms.encoding_NRZI(data)
+        elif encoding == enc_manch:
+            encoding_algorithms.encoding_MANCH(data)
+        elif encoding == enc_diff_manch:
+            encoding_algorithms.encoding_DMANCH(data)
+        elif encoding == enc_MLT3:
+            encoding_algorithms.encoding_MLT3(data)
+        elif encoding == enc_2B1Q:
+            try:
+                encoding_algorithms.encoding_2B1Q(data)
+            except ValueError as err:
+                print(err)
+                print()
+        else:
+            raise RuntimeError("You somehow got past the encoding check. Report this, please")
+
+        plt.show()
+        print()
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/encoding_algorithms.py
@@ -0,0 +1,108 @@
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+
+def setup_plot():
+    plt.margins(0.01)
+    plt.gca().xaxis.grid(True, linestyle='dashed')
+    plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(1))  # makes interval between ticks 1
+    plt.gca().set_xticklabels([])
+    plt.xlabel('Time')
+    plt.ylabel('Voltage')
+
+
+def encoding_NRZ(data):
+    data.append(data[-1])
+
+    setup_plot()
+    plt.plot(data, drawstyle='steps-post')
+    plt.title('NRZ')
+    plt.yticks([0, .5, 1], ['-U', '0V', '+U'])
+
+
+def encoding_NRZI(data):
+    level = False
+    for bit in range(0, len(data)):
+        if data[bit]:
+            level = not level
+
+        data[bit] = int(level)
+
+    setup_plot()
+    plt.plot(data, drawstyle='steps-post')
+    plt.title('NRZI')
+    plt.yticks([0, .5, 1], ['-U', '0V', '+U'])
+
+
+def encoding_MANCH(data):
+    formatted_data = [.5, .5]
+    for bit in data:
+        if bit:
+            formatted_data.append(1)
+            formatted_data.append(0)
+        else:
+            formatted_data.append(0)
+            formatted_data.append(1)
+
+    setup_plot()
+    plt.plot(formatted_data, drawstyle='steps-pre')
+    plt.title('Manchester')
+    plt.yticks([0, .5, 1], ['-U', '0V', '+U'])
+
+
+def encoding_DMANCH(data):
+    formatted_data = [.5, .5, 1]
+    for bit in data:
+        last = formatted_data[-1]
+        if bit:
+            formatted_data.append(last)
+            formatted_data.append(not last)
+        else:
+            formatted_data.append(not last)
+            formatted_data.append(last)
+
+    setup_plot()
+    plt.plot(formatted_data, drawstyle='steps-pre')
+    plt.title('Differential Manchester')
+    plt.yticks([0, .5, 1], ['-U', '0V', '+U'])
+
+
+def encoding_MLT3(data):
+    formatted_data = [.5, .5]
+    level = level_delta = .5
+    for bit in data:
+        if bit:
+            level += level_delta
+            if level > 1 or level < 0:
+                level_delta = -level_delta
+                level += 2 * level_delta
+
+        formatted_data.append(level)
+
+    setup_plot()
+    plt.plot(formatted_data, drawstyle='steps-pre')
+    plt.title('MLT-3')
+    plt.yticks([0, .5, 1], ['-U', '0V', '+U'])
+
+
+def encoding_2B1Q(data):
+    if len(data) % 2 != 0:
+        raise ValueError("Data stream length should be a multiple of 2 for 2B1Q encoding")
+
+    formatted_data = []
+
+    for pair in zip(data[0::2], data[1::2]):
+        print(pair)
+        if pair == (0, 0):
+            formatted_data.append(0)
+        elif pair == (0, 1):
+            formatted_data.append(1)
+        elif pair == (1, 1):
+            formatted_data.append(2)
+        else:
+            formatted_data.append(3)
+
+    setup_plot()
+    plt.plot(formatted_data, drawstyle='steps-pre')
+    plt.title('2B1Q')
+    plt.yticks([0, 1, 2, 3], ['-3V', '-1V', '1V', '3V'])
