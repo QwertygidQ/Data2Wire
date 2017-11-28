d1e03bef56108e44fc342b9a9eeac2fd74df184d
Ivan Lysenko <Qwertygid@gmail.com>
2017-11-28 23:22:33 +0300

    Fixed a typo

--- src/encoding_algorithms.py
+++ src/encoding_algorithms.py
@@ -5,7 +5,7 @@ import matplotlib.ticker as ticker
 def setup_plot():
     plt.margins(0.01)
     plt.gca().xaxis.grid(True, linestyle='dashed')
-    plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(1))  # makes interval between ticks 1
+    plt.gca().xaxis.set_major_locator(ticker.MultipleLocator(1))  # makes interval between ticks equal to 1
     plt.gca().set_xticklabels([])
     plt.xlabel('Time')
     plt.ylabel('Voltage')
